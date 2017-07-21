a28852b30f76a35348d045bc83a50efbef85e10a
laura <lmelgarejos@unal.edu.co>
2017-07-21 05:25:58 -0700

    Revert "Trying to fix compilation error"
    This reverts commit cd109db6527f9a0520e0da5cea1901136508e182.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import hola from './hi.js';
 
 class App extends Component {
-
-  // <script type="text/jsx">
-      /** @jsx React.DOM */
-      React.render(
-          <h1>Hello, world!</h1>,
-          document.getElementById('myDiv')
-      );
-  // </script>
-
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          {hola}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
